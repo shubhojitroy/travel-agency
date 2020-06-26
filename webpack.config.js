c919e7bc96c3432b4cf47c081e1d1f4bb962ba57
shubhojit <royshubhojit@gmail.com>
2020-06-27 06:01:55 +1000

    appended and updated styles in css modules and index HTML in travel-agency project.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require("path");
 
-const postCSSPlugins = [
+const postCSSPlugins = [  
   require('postcss-import'),
+  require('postcss-mixins'),
   require("postcss-simple-vars"),
   require("postcss-nested"),
   require("autoprefixer"),
